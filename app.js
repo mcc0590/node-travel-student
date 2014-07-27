1aa95e2390f413f7f2e388fd2e5ab987bbbf3e3f
Michael Criste <michael.charlesc@gmail.com>
2014-07-27 22:03:35 +0000

    check it out

--- app.js
+++ app.js
@@ -5,10 +5,39 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "Michael Criste";
+	var myName = "Michael";
 	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
+    resp.render("index", {"name" : myName});
 	});
+  
+  app.get("/icecream/:flavor", function(res,resp){
+    resp.render("icecream", { flavor: req.params.flavor });
+  });
 
-})
+  app.get("/travel", function(req, resp) {
+    db.findArray({}, function(results) {
+      var params = {
+      "places": results 
+      }
+      
+      resp.render("travel", params);  
+    })
+    
+  });
+  
+  
+  app.get("/travel/rating/:rating", function(req, resp) {
+    var rating = parseInt(req.params.rating);
+    db.findArray({ "rating": rating }, function(results) {
+      var params = {
+      "places": results 
+      }
+      
+      resp.render("travel", params);  
+    })
+    
+  });
+  
+  
+  
+});
